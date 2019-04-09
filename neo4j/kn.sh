9bca1f8546823ea55c3ec3251f92d78a14be6174
Yang Tian <ty7181@gmail.com>
2019-04-08 22:41:30 -0400

    Delete kn.sh

--- neo4j/kn.sh
+++
@@ -1,3 +0,0 @@
-nohup python kn.py /home/ubuntu/ecosys/benchmark/neo4j/twitter_rv.net-seed 300 neo4j 1 notes latency;nohup python kn.py /home/ubuntu/ecosys/benchmark/neo4j/twitter_rv.net-seed 300 neo4j 2 notes latency;nohup python kn.py /home/ubuntu/ecosys/benchmark/neo4j/twitter_rv.net-seed 10 neo4j 3 notes latency;nohup python kn.py /home/ubuntu/ecosys/benchmark/neo4j/twitter_rv.net-seed 10 neo4j 6 notes latency
-
-
